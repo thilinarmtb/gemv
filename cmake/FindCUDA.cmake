75dc99a39e5df82d458264b2ea520a0af4a32876
Thilina Rathnayake <thilinarmtb@gmail.com>
2024-02-11 10:55:58 -0600

    Start on a native CUDA backend

--- cmake/FindCUDA.cmake
+++ cmake/FindCUDA.cmake
@@ -2,8 +2,10 @@ find_package(CUDAToolkit)
 
 if (TARGET CUDA::toolkit)
   add_library(gemv::CUDA INTERFACE IMPORTED)
-  target_link_libraries(gemv::CUDA INTERFACE CUDA::cudart CUDA::cuda_driver
-    CUDA::cublas)
+  target_link_libraries(gemv::CUDA INTERFACE CUDA::cudart CUDA::cuda_driver)
+  if (TARGET CUDA::cublas)
+    target_link_libraries(gemv::CUDA cuda::cublas)
+  endif()
 endif()
 
 if (TARGET gemv::CUDA AND "${GEMV_DEFAULT_BACKEND}" STREQUAL "")
--- src/gemv-cublas.c
+++ src/gemv-cublas.c
@@ -1,3 +1,4 @@
+#include <cublas_v2.h>
 #include <cuda_runtime.h>
 
 #include "gemv-impl.h"
@@ -12,22 +13,28 @@ static inline void check_cuda_runtime_(cudaError_t err, const char *file,
 
 #define check_cuda_runtime(call) check_cuda_runtime_((call), __FILE__, __LINE__)
 
+static cublasHandle_t handle = NULL;
 static float *d_A = NULL;
 static int n = 0;
 
-static void cuda_init(int device, int n_, const float *A) {
+static void cublas_init(int device, int n_, const float *A) {
   check_cuda_runtime(cudaSetDevice(device));
   n = n_;
 
   check_cuda_runtime(cudaMalloc((void **)&d_A, n * n * sizeof(float)));
   check_cuda_runtime(
       cudaMemcpy(d_A, A, n * n * sizeof(float), cudaMemcpyHostToDevice));
+
+  cublasCreate(&handle);
 }
 
-static void cuda_gemv(float *d_y, const float *d_x) {}
+static void cublas_gemv(float *d_y, const float *d_x) {
+  float alpha = 1.0f, beta = 0.0f;
+  cublasSgemv(handle, CUBLAS_OP_T, n, n, &alpha, d_A, n, d_x, 1, &beta, d_y, 1);
+}
 
-static void cuda_copy(void *dest, const void *src, size_t count,
-                      gemv_direction_t direction) {
+static void cublas_copy(void *dest, const void *src, size_t count,
+                        gemv_direction_t direction) {
   enum cudaMemcpyKind kind = cudaMemcpyDefault;
   switch (direction) {
   case GEMV_D2H:
@@ -41,10 +48,12 @@ static void cuda_copy(void *dest, const void *src, size_t count,
   check_cuda_runtime(cudaMemcpy(dest, src, count, kind));
 }
 
-static void cuda_finalize(void) {
+static void cublas_finalize(void) {
   check_cuda_runtime(cudaFree(d_A)), d_A = NULL;
+  cublasDestroy(handle), handle = NULL;
 }
 
 void gemv_register_cuda(void) {
-  gemv_register_backend("cuda", cuda_init, cuda_copy, cuda_gemv, cuda_finalize);
+  gemv_register_backend("cublas", cublas_init, cublas_copy, cublas_gemv,
+                        cublas_finalize);
 }
