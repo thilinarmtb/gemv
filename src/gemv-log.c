02c422e6abe38cdf9eb721e4ba9fb54d9121ae26
Thilina Ratnayaka <thilinarmtb@gmail.com>
2024-02-29 15:06:19 -0500

    Refactor `gemv_log()` output

--- src/gemv-log.c
+++ src/gemv-log.c
@@ -1,10 +1,12 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "gemv-impl.h"
 
 static gemv_verbose_t log_level = 0;
+static const char *log_level_to_str[] = {"INFO", "WARNING", "ERROR"};
 
 void gemv_set_verbose_impl(const gemv_verbose_t level) {
   if (level < GEMV_MUTE || level > GEMV_ERROR) {
@@ -16,13 +18,19 @@ void gemv_set_verbose_impl(const gemv_verbose_t level) {
 }
 
 void gemv_log(const gemv_verbose_t level, const char *fmt, ...) {
-  if (level < log_level) return;
+  if (level <= GEMV_MUTE || level < log_level || level > GEMV_ERROR) return;
+
+  char *fmt1 = gemv_calloc(char, 32 + strnlen(fmt, BUFSIZ));
+  snprintf(fmt1, BUFSIZ, "[%s] ", log_level_to_str[level - 1]);
+  fmt1 = strncat(fmt1, fmt, BUFSIZ);
 
   va_list args;
   va_start(args, fmt);
   char buf[BUFSIZ];
-  vsnprintf(buf, BUFSIZ, fmt, args);
+  vsnprintf(buf, BUFSIZ, fmt1, args);
   va_end(args);
+
+  gemv_free(&fmt1);
 
   fprintf(stderr, "%s\n", buf);
   fflush(stderr);
